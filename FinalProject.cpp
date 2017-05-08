c75d481ea38a4abef1bd0bcf3f61165167d78e0c
Christopher E Jackson <christopherjackson@nxnode-0.cse.unr.edu>
2017-05-08 16:57:22 -0700

    First Commit 5/8 CJ

--- FinalProject.cpp
+++ FinalProject.cpp
@@ -1,5 +1,5 @@
 //Chris Jackson & Jan Lao
-//ME 493 
+//ME 493
 //Final Project
 //May 11, 2017
 
@@ -10,6 +10,7 @@
 #include <vector>
 #include <random>
 #include <time.h>
+#include <assert.h>
 
 using namespace std;
 
@@ -21,6 +22,11 @@ public:
 	double y_loc;
 	double x_loc_initial;
 	double y_loc_initial;
+	int Action;
+	int GoHome;
+
+	int x_max = 100;
+	int y_max = 100;
 
 	void init();
 	void Simulate();
@@ -28,8 +34,7 @@ public:
 };
 
 void Agent::init(){
-	int x_max = 10;
-	int y_max = 10;
+
 	x_loc_initial = rand()%x_max;
 	y_loc_initial = rand()%y_max;
 	x_loc = (double)x_loc_initial;
@@ -41,6 +46,43 @@ void Agent::Reset(){
 	y_loc = y_loc_initial;
 }
 
+void Agent::Simulate(){
+	while((Action == 0)&&(y_loc == y_max-1)){
+		Action++;
+		cout << Action << "\t" << "0" << endl;
+	}
+	while((Action == 1)&&(x_loc == x_max-1)){
+		Action++;
+		cout << Action << "\t" << "1" << endl;
+		}
+	while((Action == 2)&&(y_loc < 1 )){
+		Action++;
+		cout << Action << "\t" << "2" << endl;
+	}
+	while((Action == 3)&&(x_loc < 1)){
+		Action++;
+		cout << Action << "\t" << "3" << endl;
+	}
+	if(Action == 0){
+		y_loc++;
+	}
+	else if(Action == 1){
+		x_loc++;
+	}
+	else if(Action == 2){
+		y_loc--;
+	}
+	else if(Action == 3){
+		x_loc--;
+	}
+	else if(Action ==4){
+		//Do nothing
+	}
+	else if(Action == 5){
+		GoHome = 1;
+	}
+}
+
 /////////////////////////////////////////////////////////////
 //GridWorld
 
@@ -48,20 +90,20 @@ class GridWorld{
 public:
     int xmapdim;
     int ymapdim;
-    
+
     int countervalue;
-    
+
     void mapinit();
     vector<int> xrowcounter; //x-values are pushed into this first. Then the rows are pushed in.
     vector<vector<int>> gridcounter; //Made up of rows made up of xdim's.
-    
+
     void gridcountereval(int x_loc, int y_loc);
 };
 
 void GridWorld::mapinit(){
     xmapdim = 100;
     ymapdim = 100;
-    
+
     for(int j=0; j<ymapdim; j++){
         for (int i=0; i<xmapdim; i++){
             xrowcounter.push_back(0);
@@ -73,10 +115,10 @@ void GridWorld::mapinit(){
 void GridWorld::gridcountereval(int x_loc, int y_loc){
     //Set
     countervalue = gridcounter.at(y_loc).at(x_loc); //grid->into rows->into cells
-    
+
     //Change
     countervalue += 1;
-    
+
     //Update
     gridcounter.at(y_loc).at(x_loc) = countervalue;
 }
@@ -100,15 +142,20 @@ public:
 void Policy::init(){
 	for (int i = 0; i < 1000; i++){
 		int options = rand()%10;
-		if (options > 6){
+		if (options < 9){
 			int q = rand()%5;
 			Actions.push_back(q);
 		}
-		else if (options <= 6){
+		else if (options >= 9){
 			int q = rand()%6;
 			Actions.push_back(q);
 		}
 	}
+}
+
+void Policy::eval(){
+	Time++;
+	Exploration = Exploration + 1;
 }
 
 
@@ -120,23 +167,46 @@ void Policy::init(){
 int main(){
 	srand(time(NULL));
 	int pop_size = 100;
+	int num_gen = 300;
+
+	int counter2 = 0;
 
 	GridWorld Map;
     	Map.mapinit();
-	
+
 	Agent A;
 	A.init();
-	
+
 	Policy P;
+
 	vector<Policy> Population;
 	for (int i = 0; i < pop_size; i++){
+		Policy P;
 		P.init();
 		Population.push_back(P);
+		cout << Population.at(i).Actions.size() << endl;
+	}
+	for (int g = 0; g < num_gen; g++){
+		for (int i = 0; i < pop_size; i++){
+			int counter1 = 0;
+			cout << "get here?" << endl;
+			for (int j = 0; j < Population.at(i).Actions.size(); j++){
+				A.Action = Population.at(i).Actions.at(j);
+				A.GoHome = 0;
+				A.Simulate();
+				//Population.at(i).Actions.at(j) = A.Action;
+				cout << A.x_loc << "\t" << A.y_loc << "\t" << A.Action << "\t" << counter1++ << "\t" << g << "\t" << i << endl;
+				//Somewhere in the loop operation when the simulation makes the agent travel to a specific location...
+				Map.gridcountereval(A.x_loc, A.y_loc); //Here: x_loc and y_loc refer to the
+				P.eval();
+				if(A.GoHome == 1){
+					break;
+				}
+			}
+		}
 	}
 
 
-	//Somewhere in the loop operation when the simulation makes the agent travel to a specific location...
-    	Map.gridcountereval(A.x_loc, A.y_loc); //Here: x_loc and y_loc refer to the
 
 	return 0;
 }
